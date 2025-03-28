0becb12c1dc4e94f4fdce11aa0025fca1b206933
Roberto Sales <rsalesc@Robertos-MacBook-Pro.local>
2025-03-28 15:48:40 -0300

    Add --input and --output flags to rbx tc view.

--- rbx/box/testcases/main.py
+++ rbx/box/testcases/main.py
@@ -3,7 +3,7 @@ from typing import Annotated, List, Optional
 
 import typer
 
-from rbx import annotations, config
+from rbx import annotations, config, utils
 from rbx.box import package
 from rbx.box.generators import (
     GenerationTestcaseEntry,
@@ -32,12 +32,19 @@ def _should_generate_output(entry: GenerationTestcaseEntry) -> bool:
     ) and package.get_main_solution() is not None
 
 
-def _generate_input_for_editing(entry: GenerationTestcaseEntry) -> pathlib.Path:
-    if _should_generate_output(entry) or entry.metadata.copied_from is None:
+def _generate_input_for_editing(
+    entry: GenerationTestcaseEntry,
+    output: bool = True,
+    progress: Optional[utils.StatusProgress] = None,
+) -> pathlib.Path:
+    if (
+        output and _should_generate_output(entry)
+    ) or entry.metadata.copied_from is None:
         generate_standalone(
             entry.metadata,
             validate=False,
             group_entry=entry.group_entry,
+            progress=progress,
         )
     if entry.metadata.copied_from is not None:
         return entry.metadata.copied_from.inputPath
@@ -46,6 +53,7 @@ def _generate_input_for_editing(entry: GenerationTestcaseEntry) -> pathlib.Path:
 
 def _generate_output_for_editing(
     entry: GenerationTestcaseEntry,
+    progress: Optional[utils.StatusProgress] = None,
 ) -> Optional[pathlib.Path]:
     if (
         entry.metadata.copied_from is not None
@@ -54,15 +62,24 @@ def _generate_output_for_editing(
         return entry.metadata.copied_from.outputPath
     if not _should_generate_output(entry):
         return None
-    generate_outputs_for_testcases([entry.group_entry])
+    generate_outputs_for_testcases([entry.group_entry], progress=progress)
     return entry.metadata.copied_to.outputPath
 
 
-def _generate_for_editing(entry: GenerationTestcaseEntry) -> List[pathlib.Path]:
-    res = [_generate_input_for_editing(entry)]
-    output = _generate_output_for_editing(entry)
-    if output is not None:
-        res.append(output)
+def _generate_for_editing(
+    entry: GenerationTestcaseEntry,
+    input: bool,
+    output: bool,
+    progress: Optional[utils.StatusProgress] = None,
+) -> List[pathlib.Path]:
+    res = []
+    input_path = _generate_input_for_editing(entry, output=output, progress=progress)
+    if input:
+        res.append(input_path)
+    if output:
+        output_path = _generate_output_for_editing(entry, progress=progress)
+        if output_path is not None:
+            res.append(output_path)
     return res
 
 
@@ -72,9 +89,30 @@ def view(
         str,
         typer.Argument(help='Testcase to view. Format: [group]/[index].'),
     ],
+    input_only: bool = typer.Option(
+        False,
+        '--input',
+        '-i',
+        help='Whether to open only the input file in the editor.',
+    ),
+    output_only: bool = typer.Option(
+        False,
+        '--output',
+        '-o',
+        help='Whether to open only the output file in the editor.',
+    ),
 ):
+    if input_only and output_only:
+        console.print(
+            '[error]Flags --input and --output cannot be used together.[/error]'
+        )
+        raise typer.Exit(1)
+
     entry = TestcaseEntry.parse(tc)
     testcase = _find_testcase(entry)
 
-    items = _generate_for_editing(testcase)
+    with utils.StatusProgress('Preparing testcase...') as s:
+        items = _generate_for_editing(
+            testcase, input=not output_only, output=not input_only, progress=s
+        )
     config.edit_multiple(items)
