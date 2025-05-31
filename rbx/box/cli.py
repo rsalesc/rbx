18b2d70254787e6fc642b89196dda8bd9e98cfda
Roberto Sales <rsalesc@Robertos-MacBook-Pro.local>
2025-05-30 22:14:31 -0300

    Change default behavior for capturing logs in interactive problems.

--- rbx/box/cli.py
+++ rbx/box/cli.py
@@ -104,12 +104,10 @@ def main(
         help='Whether to compile and run testlib components with sanitizers enabled. '
         'If you want to run the solutions with sanitizers enabled, use the "-s" flag in the corresponding run command.',
     ),
-    debug_logs: bool = typer.Option(
-        False,
-        '--debug-logs',
-        '--debug',
-        '-d',
-        help='Whether to save extra debug logs along with the evaluation results.',
+    capture: bool = typer.Option(
+        True,
+        '--capture',
+        help='Whether to save extra logs and outputs from interactive solutions.',
     ),
 ):
     if cd.is_problem_package() and not package.is_cache_valid():
@@ -125,7 +123,7 @@ def main(
             '[warning]Sanitizers are running just for testlib components.\n'
             'If you want to run the solutions with sanitizers enabled, use the [item]-s[/item] flag in the corresponding run command.[/warning]'
         )
-    state.STATE.debug_logs = debug_logs
+    state.STATE.debug_logs = capture
 
 
 @app.command('ui', hidden=True)
