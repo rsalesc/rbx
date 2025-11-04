5fecc9f4c0864cc456b3388031fb0fbef3abbb54
rsalescaldeira@gmail.com <rsalesc@Robertos-MacBook-Pro.local>
2025-11-04 20:09:24 -0300

    fix: handle ctrl+C in stress tests

--- rbx/box/stresses.py
+++ rbx/box/stresses.py
@@ -139,187 +139,192 @@ async def run_stress(
     skipped = 0
     findings = []
 
-    while len(findings) < findingsLimit:
-        if time.monotonic() - startTime > timeoutInSeconds:
-            break
+    try:
+        while len(findings) < findingsLimit:
+            if time.monotonic() - startTime > timeoutInSeconds:
+                break
 
-        if print_descriptors:
-            utils.print_open_fd_count()
+            if print_descriptors:
+                utils.print_open_fd_count()
 
-        executed += 1
+            executed += 1
 
-        if progress:
-            seconds = timeoutInSeconds - int(time.monotonic() - startTime)
-            skipped_str = f'skipped [item]{skipped}[/item], ' if skipped else ''
-            progress.update(
-                f'Stress testing: found [item]{len(findings)}[/item] tests, '
-                f'executed [item]{executed}[/item], '
-                f'{skipped_str}'
-                f'[item]{seconds}[/item] second(s) remaining...'
-            )
-
-        input_path = runs_dir / '.stress' / 'input'
-        input_path.parent.mkdir(parents=True, exist_ok=True)
-
-        expanded_generator_call = expand_generator_call(stress.generator)
-        try:
-            await generate_standalone(
-                GenerationMetadata(
-                    generator_call=expanded_generator_call,
-                    copied_to=Testcase(inputPath=input_path),
-                ),
-                generator_digest=generator_digest,
-                validators_digests=validators_digests,
-            )
-        except (ValidationError, GenerationError) as err:
-            if skip_invalid_testcases:
-                skipped += 1
-                continue
-            with err:
-                err.print('[warning]Invalid testcase generated.[/warning]')
-                err.print(
-                    '[warning]You can use the [item]--skip[/item] flag to skip invalid testcases without halting.[/warning]'
+            if progress:
+                seconds = timeoutInSeconds - int(time.monotonic() - startTime)
+                skipped_str = f'skipped [item]{skipped}[/item], ' if skipped else ''
+                progress.update(
+                    f'Stress testing: found [item]{len(findings)}[/item] tests, '
+                    f'executed [item]{executed}[/item], '
+                    f'{skipped_str}'
+                    f'[item]{seconds}[/item] second(s) remaining...'
                 )
 
-        @async_lru.alru_cache(maxsize=None)
-        async def run_solution_fn(
-            solution: str,
-            checker_digest: Optional[str] = None,
-            input_path=input_path,
-            output_path: Optional[pathlib.Path] = None,
-        ) -> Evaluation:
-            index = solution_indices[solution]
-            sol = solutions[index]
-            return await tasks.run_solution_on_testcase(
-                solutions[index],
-                compiled_digest=solutions_digest[sol.path],
-                checker_digest=checker_digest,
-                interactor_digest=interactor_digest,
-                testcase=Testcase(inputPath=input_path, outputPath=output_path),
-                output_dir=input_path.parent,
-                filestem=f'{index}',
-                is_stress=True,
-            )
+            input_path = runs_dir / '.stress' / 'input'
+            input_path.parent.mkdir(parents=True, exist_ok=True)
 
-        # Get main solution output.
-        expected_output_path = empty_path
-        if needs_expected_output:
-            eval = await run_solution_fn(str(solutions[0].path))
-            if eval.result.outcome != Outcome.ACCEPTED:
-                console.console.print(
-                    '[error]Error while generating main solution output.[/error]'
+            expanded_generator_call = expand_generator_call(stress.generator)
+            try:
+                await generate_standalone(
+                    GenerationMetadata(
+                        generator_call=expanded_generator_call,
+                        copied_to=Testcase(inputPath=input_path),
+                    ),
+                    generator_digest=generator_digest,
+                    validators_digests=validators_digests,
                 )
-                console.console.print(f'Input written at [item]{input_path}[/item]')
-                console.console.print(
-                    f'Output written at [item]{eval.log.stdout_absolute_path}[/item]'
-                )
-                console.console.print(
-                    f'Stderr written at [item]{eval.log.stderr_absolute_path}[/item]'
-                )
-                console.console.print()
-                console.console.print(
-                    "[warning]If you don't want reference outputs to be generated for the tests, you should "
-                    "use the two-way modifier in your finder expression (':2')."
-                )
-                raise typer.Exit(1)
-            if eval.log.stdout_absolute_path is not None:
-                expected_output_path = input_path.with_suffix('.ans')
-                shutil.copyfile(eval.log.stdout_absolute_path, expected_output_path)
-            else:
-                expected_output_path = None
-
-        @async_lru.alru_cache(maxsize=None)
-        async def run_solution_and_checker_fn(
-            call: finder_parser.FinderCall,
-            expected_output_path=expected_output_path,
-        ) -> finder_parser.FinderResult:
-            async def run_fn() -> Evaluation:
-                solution = call.solution
-                checker = call.checker
-
-                checker_digest = (
-                    finders_digest[checker.path] if checker is not None else None
-                )
-                return await run_solution_fn(
-                    solution,
-                    checker_digest=checker_digest,
-                    output_path=expected_output_path,
-                )
-
-            eval = await run_fn()
-
-            return finder_parser.FinderResult(
-                solution=call.solution,
-                outcome=eval.result.outcome,
-                checker=call.checker,
-                solution_log=eval.log,
-                checker_result=eval.result,
-            )
-
-        @syncer.sync
-        async def run_fn(*args, **kwargs):
-            # Wrap the runner in a syncer.sync to make it work with the finder parser.
-            return await run_solution_and_checker_fn(*args, **kwargs)
-
-        runner = finder_parser.FinderTreeRunner(runner=run_fn)
-        finder_outcome: finder_parser.FinderOutcome = runner.transform(parsed_finder)
-
-        internal_error_results = [
-            result
-            for result in finder_outcome.results
-            if result.outcome == Outcome.INTERNAL_ERROR
-        ]
-
-        if internal_error_results:
-            console.console.print(
-                f'[error]Checkers failed during stress test [item]{stress.name}[/item] with args [info]{expanded_generator_call.name} {expanded_generator_call.args}[/info][/error]'
-            )
-            for internal_error_result in internal_error_results:
-                assert internal_error_result.checker is not None
-                assert internal_error_result.checker_result is not None
-                internal_error_checker_name = internal_error_result.checker.path
-                console.console.print(
-                    f'[warning]Checker [item]{internal_error_checker_name}[/item] failed with message:'
-                )
-                console.console.print(internal_error_result.checker_result.message)
-            raise typer.Exit(1)
-
-        if not finder_outcome.truth_value:
-            continue
-
-        findings_dir = stress_dir / 'findings'
-        findings_dir.mkdir(parents=True, exist_ok=True)
-        finding_index = len(findings)
-
-        finding_path = findings_dir / f'{finding_index}.in'
-        finding_path.write_bytes(input_path.read_bytes())
-
-        if progress:
-            console.console.print(
-                f'[error]FINDING[/error] Generator args are "[status]{expanded_generator_call.name} {expanded_generator_call.args}[/status]"'
-            )
-            seen_finder_results = set()
-            for finder_result in finder_outcome.results:
-                style = get_outcome_style_verdict(finder_result.outcome)
-                finder_result_key = (finder_result.solution, finder_result.checker)
-                if finder_result_key in seen_finder_results:
+            except (ValidationError, GenerationError) as err:
+                if skip_invalid_testcases:
+                    skipped += 1
                     continue
-                seen_finder_results.add(finder_result_key)
-                finder_result_report_line = f'{finder_result.solution} = [{style}]{finder_result.outcome.name}[/{style}]'
-                if finder_result.checker is not None:
-                    finder_result_report_line += (
-                        f' [item]ON[/item] {finder_result.checker.path}'
+                with err:
+                    err.print('[warning]Invalid testcase generated.[/warning]')
+                    err.print(
+                        '[warning]You can use the [item]--skip[/item] flag to skip invalid testcases without halting.[/warning]'
                     )
-                console.console.print(finder_result_report_line)
 
-        findings.append(
-            StressFinding(
-                generator=expanded_generator_call,
+            @async_lru.alru_cache(maxsize=None)
+            async def run_solution_fn(
+                solution: str,
+                checker_digest: Optional[str] = None,
+                input_path=input_path,
+                output_path: Optional[pathlib.Path] = None,
+            ) -> Evaluation:
+                index = solution_indices[solution]
+                sol = solutions[index]
+                return await tasks.run_solution_on_testcase(
+                    solutions[index],
+                    compiled_digest=solutions_digest[sol.path],
+                    checker_digest=checker_digest,
+                    interactor_digest=interactor_digest,
+                    testcase=Testcase(inputPath=input_path, outputPath=output_path),
+                    output_dir=input_path.parent,
+                    filestem=f'{index}',
+                    is_stress=True,
+                )
+
+            # Get main solution output.
+            expected_output_path = empty_path
+            if needs_expected_output:
+                eval = await run_solution_fn(str(solutions[0].path))
+                if eval.result.outcome != Outcome.ACCEPTED:
+                    console.console.print(
+                        '[error]Error while generating main solution output.[/error]'
+                    )
+                    console.console.print(f'Input written at [item]{input_path}[/item]')
+                    console.console.print(
+                        f'Output written at [item]{eval.log.stdout_absolute_path}[/item]'
+                    )
+                    console.console.print(
+                        f'Stderr written at [item]{eval.log.stderr_absolute_path}[/item]'
+                    )
+                    console.console.print()
+                    console.console.print(
+                        "[warning]If you don't want reference outputs to be generated for the tests, you should "
+                        "use the two-way modifier in your finder expression (':2')."
+                    )
+                    raise typer.Exit(1)
+                if eval.log.stdout_absolute_path is not None:
+                    expected_output_path = input_path.with_suffix('.ans')
+                    shutil.copyfile(eval.log.stdout_absolute_path, expected_output_path)
+                else:
+                    expected_output_path = None
+
+            @async_lru.alru_cache(maxsize=None)
+            async def run_solution_and_checker_fn(
+                call: finder_parser.FinderCall,
+                expected_output_path=expected_output_path,
+            ) -> finder_parser.FinderResult:
+                async def run_fn() -> Evaluation:
+                    solution = call.solution
+                    checker = call.checker
+
+                    checker_digest = (
+                        finders_digest[checker.path] if checker is not None else None
+                    )
+                    return await run_solution_fn(
+                        solution,
+                        checker_digest=checker_digest,
+                        output_path=expected_output_path,
+                    )
+
+                eval = await run_fn()
+
+                return finder_parser.FinderResult(
+                    solution=call.solution,
+                    outcome=eval.result.outcome,
+                    checker=call.checker,
+                    solution_log=eval.log,
+                    checker_result=eval.result,
+                )
+
+            @syncer.sync
+            async def run_fn(*args, **kwargs):
+                # Wrap the runner in a syncer.sync to make it work with the finder parser.
+                return await run_solution_and_checker_fn(*args, **kwargs)
+
+            runner = finder_parser.FinderTreeRunner(runner=run_fn)
+            finder_outcome: finder_parser.FinderOutcome = runner.transform(
+                parsed_finder
             )
-        )
 
-        # Be cooperative.
-        time.sleep(0.001)
+            internal_error_results = [
+                result
+                for result in finder_outcome.results
+                if result.outcome == Outcome.INTERNAL_ERROR
+            ]
+
+            if internal_error_results:
+                console.console.print(
+                    f'[error]Checkers failed during stress test [item]{stress.name}[/item] with args [info]{expanded_generator_call.name} {expanded_generator_call.args}[/info][/error]'
+                )
+                for internal_error_result in internal_error_results:
+                    assert internal_error_result.checker is not None
+                    assert internal_error_result.checker_result is not None
+                    internal_error_checker_name = internal_error_result.checker.path
+                    console.console.print(
+                        f'[warning]Checker [item]{internal_error_checker_name}[/item] failed with message:'
+                    )
+                    console.console.print(internal_error_result.checker_result.message)
+                raise typer.Exit(1)
+
+            if not finder_outcome.truth_value:
+                continue
+
+            findings_dir = stress_dir / 'findings'
+            findings_dir.mkdir(parents=True, exist_ok=True)
+            finding_index = len(findings)
+
+            finding_path = findings_dir / f'{finding_index}.in'
+            finding_path.write_bytes(input_path.read_bytes())
+
+            if progress:
+                console.console.print(
+                    f'[error]FINDING[/error] Generator args are "[status]{expanded_generator_call.name} {expanded_generator_call.args}[/status]"'
+                )
+                seen_finder_results = set()
+                for finder_result in finder_outcome.results:
+                    style = get_outcome_style_verdict(finder_result.outcome)
+                    finder_result_key = (finder_result.solution, finder_result.checker)
+                    if finder_result_key in seen_finder_results:
+                        continue
+                    seen_finder_results.add(finder_result_key)
+                    finder_result_report_line = f'{finder_result.solution} = [{style}]{finder_result.outcome.name}[/{style}]'
+                    if finder_result.checker is not None:
+                        finder_result_report_line += (
+                            f' [item]ON[/item] {finder_result.checker.path}'
+                        )
+                    console.console.print(finder_result_report_line)
+
+            findings.append(
+                StressFinding(
+                    generator=expanded_generator_call,
+                )
+            )
+
+            # Be cooperative.
+            time.sleep(0.001)
+    except KeyboardInterrupt:
+        pass
 
     return StressReport(findings=findings, executed=executed, skipped=skipped)
 
