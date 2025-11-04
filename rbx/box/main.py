2d58735435b68be42042c9eb2b47166962900fc2
rsalescaldeira@gmail.com <rsalesc@Robertos-MacBook-Pro.local>
2025-11-04 16:25:10 -0300

    fix: ensure ctrl+C does not show stacktrace

--- rbx/box/main.py
+++ rbx/box/main.py
@@ -1,7 +1,11 @@
 # flake8: noqa
 
 import os
+import signal
 import sys
+import typer
+from rich.console import Console
+from types import FrameType
 
 
 def _check_completions():
@@ -9,8 +13,16 @@ def _check_completions():
         sys.exit(0)
 
 
+def _keyboard_interrupt_handler(signum: int, frame: FrameType | None):
+    Console().show_cursor()
+    raise typer.Abort()  # pyright: ignore[reportUndefinedVariable]
+
+
 def app():
     # _check_completions()
+
+    # TODO: do not install this handler when in dev mode
+    signal.signal(signal.SIGINT, _keyboard_interrupt_handler)
 
     from rbx.box.exception import RbxException
 
